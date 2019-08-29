f09470876d823ed8a99c46e56cb2b9b5489a07e2
tusharavi <tusharpkt@gmail.com>
2019-08-29 10:14:15 +0530

    Junit Implementation

---
+++ src/test/java/projectManagement/ProjectManagementTest.java
@@ -0,0 +1,15 @@
+package projectManagement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import projectManagement.services.EmployeeServiceImpl;
+import projectManagement.services.EmployeeServices;
+class ProjectManagementTest {
+	EmployeeServices service = new EmployeeServiceImpl();
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
