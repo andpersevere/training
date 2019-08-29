056a66fd7960ec32cde086253d5fabc0d7bdf56d
SaurabhPotdar <potdar.saurabh.12et1016@gmail.com>
2019-08-29 11:05:28 +0530

    Version 1

--- src/main/java/projectManagement/ui/MyApplication.java
+++ src/main/java/projectManagement/ui/MyApplication.java
@@ -10,6 +10,9 @@ public class MyApplication {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
+		System.out.println("Changes made");	
+		
 		int empId;
 		String empName = null;
 		double empSalary;
